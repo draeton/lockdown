35bb18fa4f36ceb5de38a27e501b066915f8ae0e
Matthew Cobbs <draeton@gmail.com>
2011-11-26 09:52:15 -0500

    start page development

--- index.md
+++ index.md
@@ -1,1 +1,82 @@
-getting started
+---
+layout: default
+title: Lockdown - Obfuscates code snippets by writing them to the canvas
+---
+
+<link rel="stylesheet" href="css/lockdown-0.0.1-min.css">
+
+<section id="main" role="main">
+
+[Lockdown](http://github.matthewcobbs.com/lockdown/) obfuscates code snippets 
+by writing them to the canvas. This demo is compatible with IE7+, Firefox, Chrome,
+and Safari.
+
+<a href="http://www.w3.org/html/logo/"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-graphics.png" height="50" alt="HTML5 Powered with CSS3 / Styling, and Graphics" title="HTML5 Powered with CSS3 / Styling, and Graphics"></a>
+
+<!-- code examples -->
+    <h2>Code 1</h2>
+    <pre class="code base64">
+function%20foo%20()%20%7B%0A%20%20alert('Hello%20world!')%0A%7D
+    </pre>
+
+    <h2>Code 2</h2>
+    <pre class="code base64">
+var%20t%20%3D%20%24(%22%3Ctextarea%3E%22).prependTo(%22body%22)%3B%0Avar%20b%20%3D%20%24(%22%3Cbutton%3Eclick%20to%20encode%3C%2Fbutton%3E%22).click(function%20()%20%7B%0A%20%20var%20val%20%3D%20t.val()%3B%0A%20%20if%20(val)%20%7B%0A%20%20%20%20t.val(encodeURIComponent(val))%3B%0A%20%20%7D%0A%7D).insertBefore(t)%3B%0A%24(%22%3Cbr%3E%22).insertBefore(t)%3B
+    </pre>
+<!-- end code examples -->
+    
+## Implementation
+
+Place jQuery, the Lockdown script and the Lockdown stylesheet on the page:
+
+{% highlight html %}
+<link rel="stylesheet" href="css/lockdown-0.0.1-min.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+<script src="js/lockdown-0.0.1-min.js"></script>
+{% endhighlight %}
+
+Once that's in place, you may replace code blocks using the `Lockdown.lock` method:
+
+{% highlight js %}
+jQuery(document).ready(function ($) {
+
+    Lockdown.configure( {} );
+    Lockdown.lock( $("pre.code") );
+
+});
+{% endhighlight %}
+    
+## Dependencies
+
+[jQuery 1.7+](http://jquery.com/), *[Flashcanvas](http://flashcanvas.net/) for older browser support*
+
+## Contributors
+
+Matthew Cobbs (matthew.cobbs@gmail.com)
+
+## License
+
+[MIT](https://raw.github.com/draeton/stitches/master/LICENSE)
+
+## Download
+
+**The latest release, 0.0.1, is [available here](dist/lockdown-0.0.1.zip).**
+
+You can download this project in either [zip](https://github.com/draeton/lockdown/zipball/master) 
+or [tar](https://github.com/draeton/lockdown/tarball/master) formats.
+
+You can also clone the project with [Git](http://git-scm.com) by running:
+
+    $ git clone git://github.com/draeton/lockdown
+
+</section>
+
+<script src="js/lockdown-0.0.1-min.js"></script>
+<script>
+jQuery(document).ready(function ($) {
+
+    Lockdown.configure( {} );
+    Lockdown.lock( $("pre.code") );
+
+});
+</script>
---
+++ js/lockdown-0.0.1.js
@@ -0,0 +1,166 @@
+/*!
+ * Lockdown.js
+ * Obfuscates code snippets by writing them to the canvas
+ * http://draeton.github.com/lockdown
+ *
+ * Copyright 2011, Matthew Cobbs
+ * Licensed under the MIT license.
+ */
+/*global jQuery, FlashCanvas*/
+(function (window, $) {
+
+     "use strict";
+
+    var document = window.document;
+
+    // configuration variables
+    var c = {
+        // font size and positioning
+        fontsize   : 12,
+        lineheight : 24,
+        font       : "12px Courier",
+        baseline   : "top",
+        yoffset    : (24 - 12) / 2 - 2,
+
+        // font colors
+        linecolor  : "blue",
+        codecolor  : "black",
+        rowcolor   : "LightCyan",
+
+        // text start position
+        top        : 10,
+        left       : 15,
+        indent     : 8,
+
+        // canvas width
+        width      : 600,
+
+        // lines to remove from code before drawing
+        check      : new RegExp("^--remove this line--")
+    };
+
+    // constructor
+    function Lockdown (options) {
+        this.configure(options);
+        return this;
+    }
+
+    // prototype
+    Lockdown.prototype = {
+        // drawing context
+        context: null,
+        
+        // update the settings object, perhaps when styling
+        // a new section of code blocks
+        "configure": function (options) {
+            this.settings = $.extend({}, c, options);
+        },
+        
+        "lock": function ($elements) {
+            var self = this;
+            
+            if ($elements) {
+                $elements.each(function () {
+                    self.replaceCodeBlock(this, this.innerText, this.className);
+                });
+            }
+        },
+
+        // decode the base64 encoded code
+        // get the code text as an array of lines
+        // filter out the garbage lines
+        "getCode": function (innerText, className) {
+            var s = this.settings;
+
+            if (/base64/.test(className)) {
+                innerText = decodeURIComponent(innerText);
+            }
+
+            var code = $.trim(innerText).split("\n");
+
+            return $(code).filter(function (i, line) {
+                return !s.check.test(line);
+            });
+        },
+
+        // create the canvas element with height for the # of lines
+        // FlashCanvas for IE support
+        "getCanvas": function (code) {
+            var s = this.settings;
+
+            var canvas = document.createElement("canvas");
+            canvas.width = s.width;
+            canvas.height = code.length * s.lineheight + s.top * 2;
+            canvas.style.width = canvas.width + "px";
+            canvas.style.height = canvas.height + "px";
+
+            if (typeof FlashCanvas !== "undefined") {
+                FlashCanvas.initElement(canvas);
+            }
+
+            return canvas;
+        },
+
+        // write a piece of text to the canvas
+        "writeText": function (text, x, y, color, align) {
+            var s = this.settings;
+
+            this.context.font         = s.font;
+            this.context.textAlign    = align || "left";
+            this.context.textBaseline = s.baseline;
+            this.context.fillStyle    = color;
+            this.context.fillText(text, x, y);
+        },
+
+        "writeTextRight": function (text, x, y, color) {
+            this.writeText(text, x, y, color, "right");
+        },
+
+        // write the line number
+        // write the code
+        "writeLine": function (i, line) {
+            var s = this.settings;
+            var linex = s.left + s.indent - c.indent;
+            var liney = i * s.lineheight + s.top + s.yoffset;
+            var codex = s.left + s.indent;
+            var codey = i * s.lineheight + s.top + s.yoffset;
+
+            if (i % 2) {
+                // draw an alternating background
+                this.context.fillStyle   = s.rowcolor;
+                this.context.fillRect(0, i * s.lineheight + s.top, s.width, s.lineheight);
+            }
+
+            this.writeTextRight(i + 1, linex, liney, s.linecolor);
+            this.writeText(line, codex, codey, s.codecolor);
+        },
+
+        // parse the code from the pre element
+        // place and get the canvas
+        // get the drawing context
+        "replaceCodeBlock": function (element, innerText, className) {
+            var s = this.settings;
+
+            // get the code from the text
+            var code = this.getCode(innerText, className);
+
+            // determine the indent of the line numbers
+            s.indent = c.indent * (code.length + "").length;
+
+            // create a canvas
+            var canvas = this.getCanvas(code);
+
+            // replace the element
+            $(element).replaceWith(canvas);
+            $(canvas).addClass("lockdown");
+
+            // get the drawing context and starting drawing lines
+            this.context = canvas.getContext("2d");
+            $(code).each($.proxy(this.writeLine, this));
+        }
+    };
+
+    // global reference
+    window.Lockdown = new Lockdown;
+
+})(window, jQuery);
